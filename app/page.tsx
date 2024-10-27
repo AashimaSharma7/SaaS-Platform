0b5d764373384237bc0bbaf521438300974a3b8d
AashimaSharma <aashima127s@gmail.com>
2024-10-27 23:50:52 +0530

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-6xl text-pink-500">Hello saas ai</p>
+    <p className="text-6xl text-pink-500">Hello saas ai. wlcm to the world</p>
   );
 }
